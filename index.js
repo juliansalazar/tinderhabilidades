cc6ce1e756298cfd20ff071050a2c179f88878a7
Julian Salazar <juliansalazarvelez94@gmail.com>
2024-02-14 15:29:27 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// 1- Importar Express
+const express = require('express');
+
+// 2- Crear el servidor
+const app = express();
+
+// 3- Configurar app express
+app.use(express.json()); // Sirve para recibir los json que envie el servidor
+app.use(express.urlencoded({ extended: true })); // Sirve para enviar los formularios
+
+// 4- Crear rutas
+app.get('/', (req, res) => {
+    res.send('Hola Mundo');
+})
+
+// 6- Importar las rutas
+
+
+// 5- Levantar el servidor
+app.listen(3001, () => {
+    console.log('Servidor corriendo en el puerto 3001');
+});
